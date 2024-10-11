bffaef4da1009a5c9e4d111675b57e0be61e38e0
Daivik Vennela <63566742+daivikvennela@users.noreply.github.com>
2024-10-11 15:18:31 -0700

    Add files via upload
    updated solutions

--- list_manip.py
+++ list_manip.py
@@ -1,27 +1,65 @@
-# Some basic list manipulation from lab
+HW_SOURCE_FILE=__file__
+
 
 def insert_items(s, before, after):
+    """Insert after into s after each occurrence of before and then return s.
+
+    >>> test_s = [1, 5, 8, 5, 2, 3]
+    >>> new_s = insert_items(test_s, 5, 7)
+    >>> new_s
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> test_s
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> new_s is test_s
+    True
+    >>> double_s = [1, 2, 1, 2, 3, 3]
+    >>> double_s = insert_items(double_s, 3, 4)
+    >>> double_s
+    [1, 2, 1, 2, 3, 4, 3, 4]
+    >>> large_s = [1, 4, 8]
+    >>> large_s2 = insert_items(large_s, 4, 4)
+    >>> large_s2
+    [1, 4, 4, 8]
+    >>> large_s3 = insert_items(large_s2, 4, 6)
+    >>> large_s3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_s3 is large_s
+    True
+    """
+    "*** YOUR CODE HERE ***"
     # loop through the list and if the item is = to before insert the number in the digit after
 
-    for i in range(len(s)):
+    i = 0
+    while i < len(s):
         if s[i] == before:
-            s.insert(i, after)
-        if before == after:
+            s.insert(i + 1, after)
+            i += 2  # Skip the newly inserted item
+        else:
             i += 1
+    return s 
 
-    return s
+    
 
 def group_by(s, fn):
-    # loop through list and caclulate the key and add it into the grouped dictionary if it is not in already 
-    grouped = {}
-    for ____ in ____:
-        key = ____
-        if key in grouped:
-            ____
-        else:
-            grouped[key] = ____
-    return grouped
+    """Return a dictionary of lists that together contain the elements of s.
+    The key for each list is the value that fn returns when called on any of the
+    values of that list.
 
+    >>> group_by([12, 23, 14, 45], lambda p: p // 10)
+    {1: [12, 14], 2: [23], 4: [45]}
+    >>> group_by(range(-3, 4), lambda x: x * x)
+    {9: [-3, 3], 4: [-2, 2], 1: [-1, 1], 0: [0]}
+    """
+    # loop through list and calculate a key for each value using fn. 
+    # check if the item is already in the list if it is then append the item
+    grouped = {}
+    for i in range(len(s)):
+        key = fn(s[i])
+        if key in grouped:
+            grouped[key].append(s[i])
+        else:
+            grouped[key] = [s[i]]
+    return grouped
 
 def partial_reverse(s, start):
     """Reverse part of a list in-place, starting with start up to the end of
@@ -36,4 +74,79 @@ def partial_reverse(s, start):
     [1, 2, 7, 6, 5, 3, 4]
     """
     "*** YOUR CODE HERE ***"
+    # loop from start to the end of the list and set the current  
+    left = start
+    right = len(s) - 1
+    
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    
+   
+
+# Tree Data Abstraction
+
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [label] + list(branches)
+
+def label(tree):
+    """Return the label value of a tree."""
+    return tree[0]
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    return tree[1:]
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
+    """
+    return not branches(tree)
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the root.
+
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
 
